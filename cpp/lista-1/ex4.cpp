ce792f78434afbe02659566832c3a1012e96d55a
joaovareis <joaoreis@usp.br>
2026-07-10 10:51:32 -0300

    cpp/lista-1 completa

---
+++ cpp/lista-1/ex4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+    std::cout<<"Nome: Joao Victor Alpohim Reis"<<std::endl;
+    std::cout<<"Numero USP: 14717878"<<std::endl;
+    std::cout<<"Curso: Engenharia Eletrica"<<std::endl;
+
+    return 0;
+}
